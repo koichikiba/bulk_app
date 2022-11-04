a2f2976c02846827b410e8e5b57f60d271cd418f
koichikiba <backy.0710@gmail.com>
2022-11-05 00:25:16 +0900

    リダイレクト先(home画面新設)の設定完了、navigationはデフォルトのまま

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -129,4 +129,16 @@
             </div>
         </div>
     </body>
+</html> --}}
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>BulkApp</title>
+</head>
+<body>
+    <img src="{{ asset('img/BulkApp.png') }}" alt="">
+</body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,12 @@ use App\Http\Controllers\PostController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function () {
+      return view('welcome');
+  });
 
-Route::get('/', [PostController::class, 'index'])
-    ->name('root');
+//   Route::get('/posts', [PostController::class, 'index'])
+//     ->name('root');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
